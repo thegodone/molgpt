2f4f60cbbb3dc2148cc8e3faeb7679ef2436df5f
guillaume godin <guillaume.godin@gmail.com>
2023-07-20 11:16:22 +0200

    remove moses dep

--- generate/utils.py
+++ generate/utils.py
@@ -3,11 +3,29 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from moses.utils import get_mol
 from rdkit import Chem
    
 import numpy as np
 import threading
+
+def get_mol(smiles_or_mol):
+    '''
+    Loads SMILES/molecule into RDKit's object
+    '''
+    if isinstance(smiles_or_mol, str):
+        if len(smiles_or_mol) == 0:
+            return None
+        mol = Chem.MolFromSmiles(smiles_or_mol)
+        if mol is None:
+            return None
+        try:
+            Chem.SanitizeMol(mol)
+        except ValueError:
+            return None
+        return mol
+    return smiles_or_mol
+
+
 
 def set_seed(seed):
     random.seed(seed)
